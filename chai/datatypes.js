eca9b4920af3076ae735fd9bae623fde465df0d0
Nishantchaudhary6 <nishantfff5@gmail.com>
2025-09-09 03:34:19 +0000

    datatypes

---
+++ chai/datatypes.js
@@ -0,0 +1,12 @@
+console.log("nishant")
+console.log(3+
+    3)
+    // the code is redable
+    // number 2 to power53
+    // string""
+    // boolean true/false
+    // null standalone value
+    // undefine
+    // symbol -unique
+    //  object
+    console.log(typeof"nishant")
---
+++ chai/test.js
@@ -0,0 +1,9 @@
+// console.log("nishant")
+const accountid=63264
+let accountemail="nnjx8y"
+var accountpassword="2r36"
+// console.log(accountid);
+
+let accontemail="mgfffff"
+var accountpassword="ggj"
+console.table([accountid,accountemail,accountpassword])
